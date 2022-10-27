1d95862f90d66d9fcfc83b05bc23527ccd3571a9
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-10-27 17:18:37 +0200

    Dijkstra's algorithm Comments

--- app/src/main/java/com/mauzerov/neighbouringmatrix/Matrix.kt
+++ app/src/main/java/com/mauzerov/neighbouringmatrix/Matrix.kt
@@ -35,10 +35,16 @@ class Matrix(val size: Int) {
         // Distance from startNode to startNode is 0
         distances[startNode] = 0
         var currentNode = startNode
+        // While there are unvisited nodes
+        // Find the closest unvisited node
         while (currentNode != endNode) {
-
+            // Mark current node as visited
             visited[currentNode] = true
+            // Get neighbours of current node
             val neighbours = getNeighbours(currentNode)
+            // Update distances to neighbours
+            // If the distance to the neighbour is shorter than the current distance
+            // Update the distance and set the previous node
             for (neighbour in neighbours) {
                 val newDistance = distances[currentNode] + matrix[currentNode][neighbour]
                 if (newDistance < distances[neighbour]) {
@@ -46,6 +52,9 @@ class Matrix(val size: Int) {
                     previous[neighbour] = currentNode
                 }
             }
+
+            // Find next node to visit
+            // Set current node to the closest unvisited node
             var minDistance = Int.MAX_VALUE
             for (i in 0 until size) {
                 if (!visited[i] && distances[i] < minDistance) {
@@ -57,6 +66,8 @@ class Matrix(val size: Int) {
         // Reconstruct path
         val path = mutableListOf<Int>()
         var node = endNode
+        // While there are previous nodes
+        // Add current node to path
         while (node != -1) {
             path.add(node)
             node = previous[node]
